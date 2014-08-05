2bb557ef465a91a238d69d9405e01e2861e95515
Dave Johnson <david.clemett.johnson@gmail.com>
2014-08-04 23:39:53 -0400

    updated index view to display employees

--- compliance/views.py
+++ compliance/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
 #from django.template import RequestContext, loader
-from compliance.models import Organization
+from compliance.models import Organization, Employee
 
 from django.shortcuts import render, get_object_or_404
 
 def index(request):
     organization_list = Organization.objects.all()
-    context = {'organization_list': organization_list}
+    organization = Organization.objects.get(pk=1)
+
+    employee_list = Employee.objects.filter(employer=organization)
+
+    context = {'employee_list': employee_list}
     return render(request,'compliance/index.html', context)
 
-def detail(request, name):
-    organization = get_object_or_404(Organization, ok=name)
-    return render(request, 'organization/detail.html', {'organization': organization})
+def detail(request, org_id):
+    organization = get_object_or_404(Organization, pk=org_id)
+    return render(request, 'compliance/detail.html', {'organization': organization})
