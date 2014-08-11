1a67429cfec46c2a64890508f48eaf78d47ebd5f
Dave Johnson <dave@ansibleworks.com>
2014-08-11 19:48:32 -0400

    Fixed typos

--- compliance/urls.py
+++ compliance/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from compliance import views
 
 urlpatterns = patterns('',
-    url(r'^import/', views.importExcel, name='import')
-    url(r'^$', views.index, name='index')    
+    url(r'^import/', views.importExcel, name='import'),
+    url(r'^$', views.index, name='index'),    
 )
