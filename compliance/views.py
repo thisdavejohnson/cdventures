8dc72e2ec7a43aa6b78aca3b13fc29ad1c0929e3
Dave Johnson <dave@ansibleworks.com>
2014-08-11 15:57:35 -0400

    Removed template loader line

--- compliance/views.py
+++ compliance/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render
 #from django.template import RequestContext, loader
 from compliance.models import Organization
 
+from django.shortcuts import render, get_object_or_404
+
 def index(request):
     organization_list = Organization.objects.all()
-    template = loader.get_template('compliance/index.html')
     context = {'organization_list': organization_list}
     return render(request,'compliance/index.html', context)
+
+def detail(request, name):
+    organization = get_object_or_404(Organization, ok=name)
+    return render(request, 'organization/detail.html', {'organization': organization})
